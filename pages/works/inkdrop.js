07b046808799f8efe71de54896c4e05abd9a1199
Efrain May <efrain_may_75@hotmail.com>
2021-10-28 13:27:54 -0500

    inkdrop.js

--- pages/works/inkdrop.js
+++ pages/works/inkdrop.js
@@ -14,29 +14,32 @@ const Work = () => {
                 <P>
                     A markdown  note-taking app with 100+ plugins, cross-platform and encrypted data sync support
                 </P>
+                <List ml={4} my={4}>
+                    <ListItem>
+                        <Meta>Website</Meta>
+                        <Link href='https://www.inkdrop.app/'>
+                        https://www.inkdrop.app/ <ExternalLinkIcon mx="2px" />
+                        </Link>
+                    </ListItem>
+                    <ListItem>
+                        <Meta>Platform</Meta>
+                        <span>Windows/macOS/Linux/IOS/Android</span>
+                    </ListItem>
+                    <ListItem>
+                        <Meta>Stack</Meta>
+                        <span>NodeJS, React, Angular, Flutter, VB.NET, Next JS</span>
+                    </ListItem>
+                    <ListItem>
+                        <Meta>BlogPost</Meta>
+                        <Link href='https://efraindev.netlify.app/' target="_blank">
+                            https://efraindev.netlify.app/
+                        </Link>
+                    </ListItem>
+                </List>
+
+                <WorkImage src="/images/works/inkdrop_01.png" alt="Inkdrop" />
+                <WorkImage src="/images/works/inkdrop_02.png" alt="Inkdrop" />
             </Container>
-            <List ml={4} my={4}>
-                <ListItem>
-                    <Meta>Website</Meta>
-                    <Link href='https://www.inkdrop.app/'>
-                    https://www.inkdrop.app/ <ExternalLinkIcon mx="2px" />
-                    </Link>
-                </ListItem>
-                <ListItem>
-                    <Meta>Platform</Meta>
-                    <span>Windows/macOS/Linux/IOS/Android</span>
-                </ListItem>
-                <ListItem>
-                    <Meta>Stack</Meta>
-                    <span>NodeJS, React, Angular, Flutter, VB.NET, Next JS</span>
-                </ListItem>
-                <ListItem>
-                    <Meta>BlogPost</Meta>
-                    <Link href='https://efraindev.netlify.app/' target="_blank">
-                        https://efraindev.netlify.app/
-                    </Link>
-                </ListItem>
-            </List>
         </Layout>
     )
 }
