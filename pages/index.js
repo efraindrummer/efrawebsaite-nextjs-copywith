e7a7be145b0526eb04ef37fc0f09ca2944930580
Efrain May <efrain_may_75@hotmail.com>
2021-11-06 23:11:51 -0600

    ajusting the index js

--- pages/index.js
+++ pages/index.js
@@ -35,7 +35,7 @@ const Page = () => {
         <Layout>
             <Container>
                 <Box borderRadius="lg" bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')} p={3}  mb={6} aling="center">
-                    Hello, I&apos;m a full-stack developer based in Cd del Carmen Mexico!
+                    Hola, soy Desarrollador Web & Mobile en Mexico!
                 </Box>
                 
                 <Box display={{md:'flex'}}>
@@ -43,19 +43,19 @@ const Page = () => {
                         <Heading as="h2" varlant="page-title">
                             Efrain May
                         </Heading>
-                        <p>Software Engineer ( Artist / Developer / Designer )</p>
+                        <p>Ingeniero En Computacion ( Artista / Programador / Javascript💛 )</p>
                     </Box>
                 </Box>
                 <Box 
                     flexShrink={0} 
-                    mt={{base: 4, md: 0}} 
+                    mt={{base: 4, md: 6}} 
                     ml={{md: 6}} 
                     align="center"
                 >
                     <Image 
                         borderColor="whiteAlpha.800" 
                         borderWidth={2} 
-                        borderStyle="solid" 
+                        borderStyle="groove" 
                         maxWidth="100px" 
                         display="inline-block"
                         borderRadius="full"
@@ -64,41 +64,107 @@ const Page = () => {
                     />
                 </Box>
 
-                <Section delay={0.1}>
+                <Section delay={0.2}>
                     <Heading as="h3" varlant="section-title">
-                        Work
+                        Sobre Mi
                     </Heading>
                     <Paragraph>
                         Efrain es un Ingeniero de Software, creador de contenido y un apasionado por la tecnologia, a lo largo del tiempo
-                        a desarrollado productos para las empresas como TuProp, Telecom Bet-Gui, actualmente es un full-stack developer en
-                        texnologias JavaScript, usando React, Next, Node y Angular, tambien desarrollo aplicaciones moviles con Flutter 
-                        & Dart, Contactame {' '}
-                        <NextLink href="/works/inkdrop">
-                            <Link>Inkdrop</Link>
-                        </NextLink>
-                        .
+                        a desarrollado productos para las empresas como TuProp, Telecom Bet-Gui, actualmente me desempeño en
+                        tecnologias JavaScript, usando React, Next, Node y Angular, tambien desarrollo aplicaciones moviles con Flutter 
+                        & Dart.
                     </Paragraph>
                     <Box align="center" my={4}>
                         <NextLink href="/works">
-                            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
-                                Mi Potafolio
+                            <Button rightIcon={<ChevronRightIcon />} colorScheme="yellow">
+                                Descargar mi CV
                             </Button>
                         </NextLink>
                     </Box>
                 </Section>
                 <Section delay={0.2}>
-                    <Heading as="h3" varlant="section-title">
-                        Bio
+                    <Heading as="h2" varlant="section-title">
+                        Estudios & Certificaciones
                     </Heading>
                     <BioSection>
-                        <BioYear>
-                            1998
-                        </BioYear>
-                        Nacio en Carmen, Campeche, Mexico!
+                        <BioYear>2021</BioYear>
+                        Cloud Computing - Google Certification
                     </BioSection>
                     <BioSection>
                         <BioYear>2021</BioYear>
-                        Ingeniero en Computacion&apos; graduado en la Universidad Autonoma del Carmen de la facultad de Ciencias de la Informacion
+                        Desarrollo de Apps Moviles - Google Certification
+                    </BioSection>
+                    <BioSection>
+                        <BioYear>2021</BioYear>
+                        Flutter Advance Course - Udemy
+                    </BioSection>
+                    <BioSection>
+                        <BioYear>2021</BioYear>
+                        Flutter: Tu guía completa de desarrollo para IOS y Android - Udemy
+                    </BioSection>
+                    <BioSection>
+                        <BioYear>2021</BioYear>
+                        Flutter Avanzado: Lleva tu conocimiento al siguiente nivel - Udemy
+                    </BioSection>
+                    <BioSection>
+                        <BioYear>2021</BioYear>
+                        React: Aplicaciones en tiempo real con Socket-io - Udemy
+                    </BioSection>
+                    <BioSection>
+                        <BioYear>2021</BioYear>
+                        React: De cero a Experto(Hooks y MERN) - Udemy
+                    </BioSection>
+                    <BioSection>
+                        <BioYear>2021</BioYear>
+                        Angular De 0 a Experto (Edicion 2021) - Udemy
+                    </BioSection>
+                    <BioSection>
+                        <BioYear>2020</BioYear>
+                        Angular Avanzado: Lleva tus bases al siguiente nivel - MEAN (Edicion 2021) - Udemy
+                    </BioSection>
+                    <BioSection>
+                        <BioYear>2020</BioYear>
+                        Visual Studio Code: Mejora tu velocidad para codificar - Udemy
+                    </BioSection>
+                    <BioSection>
+                        <BioYear>2020</BioYear>
+                        Git de principiante a experto, GitHub, GitLab, Azure, Commit - Udemy
+                    </BioSection>
+                    <BioSection>
+                        <BioYear>2020</BioYear>
+                        Javascript Moderno Guia Definitiva Construye + 15 proyectos - Udemy
+                    </BioSection>
+                    <BioSection>
+                        <BioYear>2020</BioYear>
+                        Scrum Master Certificacion - Udemy
+                    </BioSection>
+                    <BioSection>
+                        <BioYear>2020</BioYear>
+                        Programador Web: Cookies y Sesiones en PHP - Udemy
+                    </BioSection>
+                    <BioSection>
+                        <BioYear>2020</BioYear>
+                        Backend con Node JS y Express - Udemy
+                    </BioSection>
+                    <BioSection>
+                        <BioYear>2020</BioYear>
+                        Diplomado Tecnico en BIG DATA - Fundacion Carlos Slim
+                    </BioSection>
+                    <BioSection>
+                        <BioYear>2020</BioYear>
+                        Administrador de Servidores - Fundacion Carlos Slim
+                    </BioSection>
+                    <BioSection>
+                        <BioYear>2019</BioYear>
+                        Administrador de Bases de Datos - Fundacion Carlos Slim
+                    </BioSection>
+                    <BioSection>
+                        <BioYear>2019</BioYear>
+                        Curador de Datos - Fundacion Carlos Slim
+                    </BioSection>
+                    <BioSection>
+                        <BioYear>2019</BioYear>
+                        Tecnico en Redes de Datos - Fundacion Carlos Slim
                     </BioSection>
                 </Section>
                 <Section delay={0.2}>
@@ -106,17 +172,13 @@ const Page = () => {
                         I ♥
                     </Heading>
                     <Paragraph>
-                        Art, Music,{' '}
-                        <Link href="https://efraindev.netlify.app/" target="_blank">
-                            Music
-                        </Link>
-                        , Playing Drums,{' '}
+                        Arte, Musica, Gatos, Baterista.
                     </Paragraph>
                 </Section>
 
                 <Section delay={0.3}>
                     <Heading as="h3" varlant="section-title">
-                        On the web   
+                        En la Red  
                     </Heading>
                     <List>
                         <ListItem>
