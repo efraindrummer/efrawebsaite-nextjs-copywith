e2b87af57ae5756e2347b44bbc79899d2edc50f6
Efrain May <efrain_may_75@hotmail.com>
2022-01-04 21:37:49 -0600

    post check all

--- pages/posts.js
+++ pages/posts.js
@@ -1,12 +1,12 @@
-import { Container, Heading, SimpleGrid } from '@chakra-ui/react'
+import { Container, Heading, SimpleGrid, Box, Image} from '@chakra-ui/react'
 import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import { GridItem } from '../components/grid-item'
 
 import thumbCallingApp from '../public/images/contents/calling_app.jpg'
-import thumbMyDeskSetup from '../public/images/contents/youtube-my-desk-setup.jpg'
-import thumb500PaidUsers from '../public/images/contents/blog-500-paid-users.jpg'
-import thumbFinancialGoal from '../public/images/contents/blog-financial-goal.png'
+import thumbMapasApp from '../public/images/contents/mapas_app.jpg'
+import thumbFlutterBug from '../public/images/contents/flutter_bugs.jpg'
+import thumbGithub from '../public/images/contents/github.jpg'
 import thumbHowToPriceYourself from '../public/images/contents/blog-how-to-price-yourself.jpg'
 import thumb50xFaster from '../public/images/contents/youtube-50x-faster.jpg'
 
@@ -25,37 +25,23 @@ const Posts = () => {
                             href="https://www.youtube.com/watch?v=blUy-wAukSw" 
                         />
                         <GridItem 
-                            title="My desk setup (late 2020)" 
-                            thumbnail={thumbMyDeskSetup}
-                            href="https://www.youtube.com/watch?v=nCd7z8gWYF8&list=RDkTx1vpVhXOI&index=4" 
+                            title="Aplicacion de Mapas" 
+                            thumbnail={thumbMapasApp}
+                            href="https://www.youtube.com/watch?v=zCmrnaMlUvk&t=14s" 
                         />
                     </SimpleGrid>
                 </Section>
                 <Section delay={0.3}>
                     <SimpleGrid columns={[1, 2, 2]} gap={6}>
                     <GridItem
-                        title="How I’ve Attracted The First 500 Paid Users For My SaaS That Costs $5/mo"
-                        thumbnail={thumb500PaidUsers}
-                        href="https://blog.inkdrop.app/how-ive-attracted-the-first-500-paid-users-for-my-saas-that-costs-5-mo-7a5b94b8e820"
+                        title="Reparando Bugs"
+                        thumbnail={thumbFlutterBug}
+                        href="https://www.youtube.com/watch?v=cQydP1xbJeM"
                     />
                     <GridItem
-                        title="I stopped setting a financial goal for my SaaS"
-                        thumbnail={thumbFinancialGoal}
-                        href="https://blog.inkdrop.app/i-stopped-setting-a-financial-goal-for-my-saas-a92c3db65506"
-                    />
-                    </SimpleGrid>
-                </Section>
-                <Section delay={0.5}>
-                    <SimpleGrid columns={[1, 2, 2]} gap={6}>
-                    <GridItem
-                        title="How to Price Yourself as a Freelance Developer"
-                        thumbnail={thumbHowToPriceYourself}
-                        href="https://blog.inkdrop.app/how-to-price-yourself-as-a-freelance-developer-3453dfd59d91"
-                    />
-                    <GridItem
-                        title="I made my React Native app 50x faster"
-                        thumbnail={thumb50xFaster}
-                        href="https://www.youtube.com/watch?v=vj723NlrIQc"
+                        title="Github"
+                        thumbnail={thumbGithub}
+                        href="https://github.com/efraindrummer"
                     />
                     </SimpleGrid>
                 </Section>
