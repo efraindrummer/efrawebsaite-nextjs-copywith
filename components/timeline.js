f5784e530b6bb724bc66a401d003dd2bd8564b1a
efraindrummer42a <may.efrain@sycec.com.mx>
2024-09-20 13:14:37 -0600

    error ready timeline

--- components/timeline.js
+++ components/timeline.js
@@ -5,73 +5,75 @@ import { FaCircle } from "react-icons/fa";
 const MotionBox = motion(Box);
 
 export const TimelineItem = ({ year, title, description, subtitle, isRight }) => {
-  const yearColor = useColorModeValue("blue.500", "blue.300");
-  const titleColor = useColorModeValue("purple.700", "purple.300");
-
-  return (
-    <motion.div
-      initial={{ opacity: 0, y: 50 }}
-      whileInView={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.8 }}
-      viewport={{ once: true }}
-    >
-      <Flex
-        direction={isRight ? "row-reverse" : "row"}
-        align="center"
-        justify="space-between"
-        mb={12}
-        w="full"
+    // Llamar a useColorModeValue fuera de cualquier condición
+    const yearColor = useColorModeValue("blue.500", "blue.300");
+    const titleColor = useColorModeValue("purple.700", "purple.300");
+    const bgColor = useColorModeValue('gray.100', 'gray.700');
+    const borderColor = useColorModeValue('gray.100', 'gray.700');
+    const textColor = useColorModeValue("gray.700", "gray.300");
+  
+    return (
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8 }}
+        viewport={{ once: true }}
       >
-        <MotionBox
-          as={FaCircle}
-          size="12px"
-          color={yearColor}
-          mr={isRight ? 0 : 10}
-          ml={isRight ? 10 : 0}
-          whileHover={{ scale: 1.5 }}
-          transition="0.3s"
-        />
-        <Box
-          bg={useColorModeValue('gray.100', 'gray.700')}
-          p={6}
-          borderRadius="lg"
-          boxShadow="xl"
-          maxWidth="%" // Ajuste para evitar que ocupe demasiado espacio
-          textAlign={isRight ? "right" : "left"}
-          position="relative"
-          _before={{
-            content: "''",
-            position: "absolute",
-            top: "50%",
-            left: isRight ? "auto" : "-8px",
-            right: isRight ? "-8px" : "auto",
-            transform: "translateY(-50%)",
-            borderWidth: "8px",
-            borderStyle: "solid",
-            borderColor: isRight
-              ? `transparent transparent transparent ${useColorModeValue('gray.100', 'gray.700')}`
-              : `transparent ${useColorModeValue('gray.100', 'gray.700')} transparent transparent`,
-          }}
+        <Flex
+          direction={isRight ? "row-reverse" : "row"}
+          align="center"
+          justify="space-between"
+          mb={12}
+          w="full"
         >
-          <Text fontSize="lg" fontWeight="bold" color={yearColor} mb={2}>
-            {year}
-          </Text>
-          <Text fontSize="md" fontWeight="semibold" color={titleColor} mb={2} letterSpacing="wide">
-            {title}
-          </Text>
-          {subtitle && (
-            <Text fontSize="md" fontStyle="italic" color={useColorModeValue("gray.700", "gray.300")} mb={4}>
-              {subtitle}
+          <MotionBox
+            as={FaCircle}
+            size="12px"
+            color={yearColor}
+            mr={isRight ? 0 : 10}
+            ml={isRight ? 10 : 0}
+            whileHover={{ scale: 1.5 }}
+            transition="0.3s"
+          />
+          <Box
+            bg={bgColor}
+            p={6}
+            borderRadius="lg"
+            boxShadow="xl"
+            maxWidth="45%" // Ajuste para evitar que ocupe demasiado espacio
+            textAlign={isRight ? "right" : "left"}
+            position="relative"
+            _before={{
+              content: "''",
+              position: "absolute",
+              top: "50%",
+              left: isRight ? "auto" : "-8px",
+              right: isRight ? "-8px" : "auto",
+              transform: "translateY(-50%)",
+              borderWidth: "8px",
+              borderStyle: "solid",
+              borderColor: isRight ? `transparent transparent transparent ${borderColor}` : `transparent ${borderColor} transparent transparent`,
+            }}
+          >
+            <Text fontSize="lg" fontWeight="bold" color={yearColor} mb={2}>
+              {year}
             </Text>
-          )}
-          <Text fontSize="sm" color={useColorModeValue("gray.700", "gray.300")}>
-            {description}
-          </Text>
-        </Box>
-      </Flex>
-    </motion.div>
-  );
-};
+            <Text fontSize="md" fontWeight="semibold" color={titleColor} mb={2} letterSpacing="wide">
+              {title}
+            </Text>
+            {subtitle && (
+              <Text fontSize="md" fontStyle="italic" color={textColor} mb={4}>
+                {subtitle}
+              </Text>
+            )}
+            <Text fontSize="sm" color={textColor}>
+              {description}
+            </Text>
+          </Box>
+        </Flex>
+      </motion.div>
+    );
+  };
 
 export const Timeline = () => {
   return (
